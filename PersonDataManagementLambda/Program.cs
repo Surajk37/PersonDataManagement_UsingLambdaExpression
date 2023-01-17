c816d707c772a4180fa8066f8b02b9f717d9aee6
Surajk37 <surajkhot9993@gmail.com>
2023-01-17 21:03:52 +0530

    Welcome Message Added

---
+++ PersonDataManagementLambda/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonDataManagementLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Person Data Management by using Lambda Expression ");
+            Console.ReadLine();
+        }
+    }
+}
